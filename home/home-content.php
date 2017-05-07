5ee01ae912fbe2d2c4295e1f9b13f244eef040fa
Seat Sovann <sovann.seat@hotmail.com>
2017-05-07 15:27:56 +0700

    Make some change on main page
    Create css and js file for home page

---
+++ home/home-content.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h3>Hello the world !</h3>
+
+</body>
+</html>
---
+++ source/js/home.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$("#btn1").click(function(){
+
+		alert("Testing");
+	});
+
+	$("#btn-signout").click(function(){
+
+		//alert("I'll leaving you !");
+		window.open("../login","_self");
+
+	});
+
+});
